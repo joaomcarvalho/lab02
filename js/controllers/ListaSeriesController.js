e7fdcb6c6437676a1192ceba0a32b64d3e41bf16
Joao Maurício <joao.carvalho@ccc.ufcg.edu.br>
2017-06-26 17:25:17 -0300

    Varias mudanças

--- js/controllers/ListaSeriesController.js
+++ js/controllers/ListaSeriesController.js
@@ -1,22 +1,62 @@
-var listaSeries = angular.module("listaSeries",[]);
+var listaSeries = angular.module("listaSeries",['ngMaterial']);
 
-listaSeries.controller("listaSeriesCtrl", function($http) {
+listaSeries.controller("listaSeriesCtrl", function($http, ListaSeriesService, $scope, $mdDialog) {
 	var self = this;
-	var url = 'https://omdbapi.com/?s=TITULO&apikey=93330d3c&type=series'
-	self.series = [];
+	this.series = [];
+	this.seriesPerfil = [];
+	this.seriesWatchList = [];
 	
 	self.search = function(name) { 
-		var uri = encodeURI(url.replace(/TITULO/, name));
-		var promisse = $http.get(uri).then(function(info) {
-			self.series = info.data.Search;
-		}, function error(info) {
-			console.log("me lasquei");
-		})
-		return promisse;
+		var promise = ListaSeriesService.search(name);
+		promise.then(function(response){
+			self.series = response.data.Search;
+		});
+		return promise;
 	};
 
 	self.hasImagePoster = function(serie) {
 		return series.Poster !== 'N/A';
-	}	
+	};	
 
+	self.adicionarAoPerfil = function(serie) {
+		var promise = ListaSeriesService.getSingleSerie(serie);
+
+		promise.then(function(response){
+			self.seriesPerfil.push(response.data);
+		});
+	};
+
+	self.exibirPerfil = function() {
+		self.series = self.seriesPerfil;
+	};
+
+	self.removerDoPerfil = function(serie) {
+		var index = self.seriesPerfil.indexOf(serie);
+		self.seriesPerfil.splice(index,1);
+	};
+
+	self.adicionarAoWatchList = function(serie) {
+		var promise = ListaSeriesService.getSingleSerie(serie);
+
+		promise.then(function(response){
+			self.seriesWatchList.push(response.data);
+		});
+	};
+
+	self.exibirWatchList = function() {
+		self.series = self.seriesWatchList;
+	};
+
+	self.removerWatchList = function() {
+		var index = self.seriesWatchList.indexOf(serie);
+		self.seriesWatchList.splice(index,1);
+	};
+
+	self.setNota = function(serie, nota) {
+		serie.nota = nota;
+	}
+
+	self.setUltimoEpisodio = function(serie, episodio) {
+		serie.ultimoEpisodio = episodio
+	}
 });
